8c87c94aeadefc70b09bb57b0b44b62a94d423e0
SahilJangid2611 <105125368+SahilJangid2611@users.noreply.github.com>
2024-10-29 21:44:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,20 @@
- const draggables = document.querySelectorAll('.draggable');
-        
-        draggables.forEach(draggable =>{
-          draggable.addEventListener('dragstart', dragstart);
-          draggable.addEventListener('dragover', dragOver);
-          draggable.addEventListener('drop', drop);
-        });
-        
-        function dragstart(e){
-          e.dataTransfer.setData('text/plain', e.target.id);
-        }
-        
-        function dragOver(e){
-          e.preventDefault();
-        }
-        function drop(event){
-          e.preventDefault();
-          const id = event.dataTransfer.getData('text/plain');
-          const draggable = document.getElementById(id); // image 1
-          
-          const targetElement = event.target; // image 2
-          if(targetElement.classList.contains('draggable')){
-            const draggedImage = draggable.style.backgroundImage;
-            const targetImage = targetElement.style.backgroundImage;
-          }
-          
-          draggable.style.backgroundImage = targetImage;
-          targetElement.style.backgroundImage = draggedImage;
-          
-        }
-        
-        
-        
-        
+  let draggedImage = null;
+    function allowDrop(event){
+        event.preventDefault();
+    }
+    function drag(event){
+        draggedImage = event.target;
+    }
+
+    function drop(event){
+        event.preventDefault();
+        const targetContainer = event.currentTarget;
+
+       if(targetContainer !== draggedImage.parentElement){
+        const targetImage = targetContainer.querySelector('img');
+
+        const tempSrc = targetImage.src;
+        targetImage.src = draggedImage.src;
+        draggedImage.src = tempSrc;
+       }
+    }
